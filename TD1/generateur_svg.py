d975c37c674c232adad0ba548b0931215dcfd7ee
Julien Bailly <julien.bailly2@etu.univ-orleans.fr>
2025-11-18 16:56:13 +0100

    Question 2 partie 3D

--- TD1/generateur_svg.py
+++ TD1/generateur_svg.py
@@ -199,11 +199,135 @@ aux=100
 points=[(aux, aux),(-aux, aux),(-aux, -aux),(aux, -aux)]
 triangles=[0,1,2,0,2,3]
 
-dessinT2 = svgwrite.Drawing('TD1/TriangeQ1.svg', size=(1000,800))
+dessinT2 = svgwrite.Drawing('TD1/TriangeQ2.svg', size=(1000,800))
 
 i=0
-dessinT2.add(dessinT2.polygon((points[triangles[3*i]],points[triangles[3*i+1]],points[triangles[3*i+2]]), fill='blue',  opacity=0.5,stroke='black'))
+dessinT2.add(dessinT2.polygon((translater(rotation(points[triangles[3*i]],angle), trans),translater(rotation(points[triangles[3*i+1]],angle), trans),translater(rotation(points[triangles[3*i+2]],angle), trans)), fill='blue',  opacity=0.5,stroke='black'))
 i=1
-dessinT2.add(dessinT2.polygon((points[triangles[3*i]],points[triangles[3*i+1]],points[triangles[3*i+2]]), fill='blue',  opacity=0.5,stroke='black'))
+dessinT2.add(dessinT2.polygon((translater(rotation(points[triangles[3*i]],angle), trans),translater(rotation(points[triangles[3*i+1]],angle), trans),translater(rotation(points[triangles[3*i+2]],angle), trans)), fill='blue',  opacity=0.5,stroke='black'))
 
 dessinT2.save()
+
+#--------------------Triange Question 3--------------------#
+
+def compose(points,triangles):
+  liste_point=[]
+  colors=("blue","red","green","purple","yellow","white","coral","darkblue")
+  for i in range(0,len(triangles)//3):
+      print(triangles[3*i],triangles[3*i+1],triangles[3*i+2])
+      dessinT3.add(dessinT3.polygon((points[triangles[3*i]],points[triangles[3*i+1]],points[triangles[3*i+2]]), fill=colors[i],  opacity=0.5,stroke='black'))
+
+aux=100
+points=[(aux, aux),(-aux, aux),(-aux, -aux),(aux, -aux)]
+triangles=[0,1,2,0,2,3]
+
+dessinT3 = svgwrite.Drawing('TD1/TriangeQ3.svg', size=(1000,800))
+
+compose(points, triangles)
+
+dessinT3.save()
+
+#--------------------Triange Question 4/5--------------------#
+# Question 4
+# def compose(points,triangles):
+#   liste_point=[]
+#   colors=("blue","red","green","purple","yellow","white","coral","darkblue")
+#   for i in range(0,len(triangles)//3):
+#       print(triangles[3*i],triangles[3*i+1],triangles[3*i+2])
+#       dessin.add(dessin.polygon((translater(points[triangles[3*i]], trans),translater(points[triangles[3*i+1]], trans),translater(points[triangles[3*i+2]], trans)), fill=colors[i],  opacity=0.7,stroke='black'))
+
+# Question 5 
+def compose(points,triangles):
+  liste_point=[]
+  colors=("blue","red","green","purple","yellow","white","coral","darkblue")
+  for i in range(0,len(triangles)//3):
+      print(triangles[3*i],triangles[3*i+1],triangles[3*i+2])
+      dessin.add(dessin.polygon((translater(rotation(points[triangles[3*i]], angle), trans),translater(rotation(points[triangles[3*i+1]], angle), trans),translater(rotation(points[triangles[3*i+2]],angle), trans)), fill=colors[i],  opacity=0.7,stroke='black'))
+
+
+aux=100
+points=[(-100, -70),(-100, 0),(-50, 0),(0, -30),(50, 0),(100, -70),(100, 0),(0, 70)]
+triangles=[0,1,2,2,3,4,4,5,6,6,7,1]
+trans = (200, 300)
+angle = -pi/8
+dessin = svgwrite.Drawing('TD1/TriangeQ5.svg', size=(800,600))
+
+compose(points, triangles)
+
+dessin.save()
+
+#--------------------Triange Question 6--------------------#
+
+def compose(points,triangles, angle, trans, coefDilatation):
+  liste_point=[]
+  colors=("blue","red","green","purple","yellow","white","coral","darkblue")
+  for i in range(0,len(triangles)//3):
+      print(triangles[3*i],triangles[3*i+1],triangles[3*i+2])
+      dessin.add(dessin.polygon((translater(rotation(prodMatVect(Matdilatation(coefDilatation),points[triangles[3*i]]), angle), trans),
+                           translater(rotation(prodMatVect(Matdilatation(coefDilatation),points[triangles[3*i+1]]), angle), trans),
+                           translater(rotation(prodMatVect(Matdilatation(coefDilatation),points[triangles[3*i+2]]),angle), trans)), fill=colors[i],  opacity=0.7,stroke='black'))
+
+
+aux=100
+points=[(-100, -70),(-100, 0),(-50, 0),(0, -30),(50, 0),(100, -70),(100, 0),(0, 70)]
+triangles=[0,1,2,2,3,4,4,5,6,6,7,1]
+coefDilatation = 1
+dessin = svgwrite.Drawing('TD1/TriangeQ6.svg', size=(800,600))
+
+
+
+x_base, y_base = 150, 300
+espacement = 100 
+angle_increment = -pi/3
+
+for i in range(5):  
+    angle = i * angle_increment 
+
+    x_offset = x_base + i * espacement
+    y_offset = y_base
+    coefDilatation -= 0.15
+    compose(points, triangles, angle, (x_offset, y_offset), coefDilatation)
+
+dessin.save()
+
+
+#--------------------3D--------------------#
+def compose(points,triangles):
+  liste_point=[]
+  colors=("purple","coral","blue","red","green","yellow","white","darkblue")
+  for i in range(0,len(triangles)//3):
+      print(triangles[3*i],triangles[3*i+1],triangles[3*i+2])
+      dessinC1.add(dessinC1.polygon((points[triangles[3*i]],points[triangles[3*i+1]],points[triangles[3*i+2]]), fill=colors[(i%(len(colors)*2))//2],  opacity=0.5,stroke='black'))
+
+aux=100
+points=[[-aux, -aux, aux], #point 0 (face devant)
+        [-aux, aux, aux],#point 1   (face devant)
+        [aux, -aux, aux],#point 2   (face devant)
+        [aux, aux, aux],#point 3    (face devant)
+        [-aux, -aux, -aux],#point 4 (face arrière)
+        [-aux, aux, -aux],#point 5 (face arrière)
+        [aux, -aux, -aux],#point 6 (face arrière)
+        [aux, aux, -aux]#.point 7 (face arrière)
+        ]
+cube=[0,1,2,   #triangle 1 face 1 (devant)
+    1,2,3,     #triangle 2 face 1
+    4,5,6,     #triangle 1 face 2 (arriere)
+    5,6,7,     #triangle 2 face 2
+    0,1,4,     #triangle 1 face 3 (gauche)
+    1,4,5,     #triangle 2 face 3
+    2,3,6,     #triangle 1 face 4 (droite)
+    3,6,7,     #triangle 2 face 4 
+    1,3,7,     #triangle 1 face 5 (haut)
+    1,5,7,     #triangle 2 face 5
+    0,2,4,     #triangle 1 face 6 (bas)
+    2,4,6]     #triangle 2 face 6 
+
+dessinC1 = svgwrite.Drawing('TD1/CubeQ1.svg', size=(800,600))
+
+def projection(point3d):
+    return((point3d[0],point3d[1]))
+
+points_proj = [ projection(sommet) for sommet in points ]
+compose(points_proj,triangles)
+
+dessinC1.save()
